d9ae9c50447295b843c0728578174f4886ce4b8d
Wil <wilfried.vandenberghe@gmail.com>
2020-07-23 18:35:35 +0200

    First JUnit 5 Kotlin test

--- src/test/java/com/github/urlshorten/_5/UrlRepositoryTest.java
+++ src/test/java/com/github/urlshorten/_5/UrlRepositoryTest.java
@@ -3,15 +3,12 @@ package com.github.urlshorten._5;
 import com.github.urlshorten.UrlRepository;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @Tag("db")
-@ExtendWith(SpringExtension.class)
 @DataJpaTest
 class UrlRepositoryTest {
 
---
+++ src/test/kotlin/com/github/urlshorten/junit5/UrlRepositioryTest.kt
@@ -0,0 +1,27 @@
+package com.github.urlshorten.junit5
+
+import com.github.urlshorten.UrlRepository
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+
+@Tag("db")
+@DataJpaTest
+open class UrlRepositioryTest(@Autowired val repository: UrlRepository) {
+
+    @Test
+    fun `should find by shortern url`() {
+        // given
+        val shortenUrl = "http://google.com/1234";
+
+        // when
+        val url = repository.findByShortenUrl(shortenUrl);
+
+        // then
+        assertTrue(url.isPresent);
+        assertEquals(shortenUrl, url.get().shortenUrl);
+    }
+}
